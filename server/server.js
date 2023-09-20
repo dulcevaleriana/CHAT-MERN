e8f0da6535e6552de938eeebc88f116318797bc9
dulce-guzman-azumo <dulce.guzman@azumo.co>
2023-09-20 16:24:25 -0400

    add dummy chats data to proof

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+
 const app = express();
+dotenv.config();
 
-app.get("/",(req,res,next)=>{
+const PORT = process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
     res.send("API is runing")
-})
+});
 
-app.get("/api/chat",(req,res,next)=>{
-    res.send("you're on view /api/chat")
-})
+app.get("/api/chat",(req,res)=>{
+    res.send(chats);
+});
 
-app.listen(5000, console.log("Server start on PORT 5000"))
+app.get("/api/chat/:id",(req,res)=>{
+    const singleChat = chats.find((c)=> c._id === req.params.id);
+    res.send(singleChat)
+});
+
+app.listen(PORT, console.log(`Server start on PORT ${PORT}`))
