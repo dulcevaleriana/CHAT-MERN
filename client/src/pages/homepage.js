3d1c6bc88ba7f9d8b797b30f465b3e379ae3367d
dulce-guzman-azumo <dulce.guzman@azumo.co>
2023-09-30 19:30:35 -0400

    add first component to login / sing up account

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,9 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Container,
+  Box,
+  Text,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  FormHelperText,
+  Button,
+} from "@chakra-ui/react";
 
-const Homepage = () =>{
-    return <div>
-        Home
-    </div>
-}
+const Homepage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const isErrorEmail = email === "";
+  const isErrorPassword = password === "";
+
+  return (
+    <Container maxW="xl" centerContent className="class-login">
+      <Box>
+        <Text>Chat App</Text>
+      </Box>
+      <form>
+        <FormControl>
+          <FormLabel>Email address</FormLabel>
+          <Input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {!isErrorEmail ? (
+            <FormHelperText>Enter your email account.</FormHelperText>
+          ) : (
+            <FormErrorMessage>Email is required.</FormErrorMessage>
+          )}
+        </FormControl>
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {!isErrorPassword ? (
+            <FormHelperText>Enter your password.</FormHelperText>
+          ) : (
+            <FormErrorMessage>password is required.</FormErrorMessage>
+          )}
+        </FormControl>
+        <Button
+          mt={4}
+          colorScheme="teal"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
+  );
+};
 
 export default Homepage;
